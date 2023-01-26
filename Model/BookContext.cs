9df70cebbb6e92e00bf94ae687f7517a35880055
Vincy de Tarsis <vincy.tomaz@cocatrel.com.br>
2023-01-26 07:34:23 -0300

    Initial commit

---
+++ Model/BookContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Model
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> optinons) : base(optinons)
+        {
+
+        }
+    }
+}
